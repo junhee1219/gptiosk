ff0fc1fb12d4fdeec4a94a9be8e647496b54bc36
jhlee3 <jhlee3@businessinsight.co.kr>
2023-07-12 09:10:07 +0900

    function add

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
-$(document).ready(function() {
-    $('#message-input').keypress(function(e) {
-      if (e.which === 13) { // 13은 엔터 키의 keyCode입니다.
-        $('#send-button').click();
-        return false; // 폼 전송 방지
-      }
-    });
-  
-    $('#send-button').click(function() {
-      var message = $('#message-input').val();
-      if (message.trim() !== '') {
-        var messageElement = $('<div>').text(message);
-        $('#chat-messages').append(messageElement);
-        $('#message-input').val('');
-      }
-    });
+var clickTimeout;
+var checkTime = 2;
+
+$(document).ready(function () {
+  // 엔터키 입력했을때 send-button 클릭이벤트
+  $("#message-input").keypress(function (e) {
+    if (e.which === 13) {
+      $("#send-button").click();
+      return false;
+    }
   });
-  
+
+  // send-button click 이벤트
+  $("#send-button").click(function () {
+    var message = $("#message-input").val();
+    message = "나 : " + message;
+    if (message.trim() !== "") {
+      clearTimeout(clickTimeout); // 클릭 타이머 초기화
+      var messageElement = $("<div>").text(message); // 메세지 채팅창에 보이기
+      $("#chat-messages").append(messageElement); // 메세지 채팅창에 보이기
+      $("#message-input").val(""); // 메세지 input창 clear
+      clickTimeout = setTimeout(sendDataToAPI, checkTime * 1000); // checkTime 초동안 입력없으면 API 호출
+    }
+  });
+  sendDataToAPI("http://example.com/api", { key: "value" }, handleAPIResponse); // url, data, callback함수
+});
+
+function sendDataToAPI(url, data, callback) {
+  $.ajax({
+    url: url,
+    method: "POST",
+    data: data,
+    success: function (response) {
+      callback(response);
+    },
+    error: function (error) {
+      callback(error);
+    },
+  });
+}
+
+function handleAPIResponse(response) {
+  console.log("API 응답:", response);
+}
+
+function addBadge(cls, content) {
+  if (cls == "" || cls == undefined) {
+    $(".chat-header").append('<div class="badge">' + content + "</div>");
+  } else {
+    $("." + cls).append('<div class="badge">' + content + "</div>");
+  }
+}
