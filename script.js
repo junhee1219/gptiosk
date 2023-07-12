062abbe935f69a5bd3fcc5506e97c7a253be7c68
jhlee3 <jhlee3@businessinsight.co.kr>
2023-07-12 17:10:56 +0900

    english prompt

--- script.js
+++ script.js
@@ -43,19 +43,19 @@ $(document).ready(function () {
 });
 
 function handleAPIResponse(response) {
-  if (response.includes("B")) {
+  if (response.includes("ㄴ")) {
     addBadge("", "메뉴선택");
   }
 
-  if (response.includes("C")) {
+  if (response.includes("ㄷ")) {
     addBadge("", "사이즈");
   }
 
-  if (response.includes("D")) {
+  if (response.includes("ㄹ")) {
     addBadge("", "결제수단");
   }
 
-  if (response.includes("E")) {
+  if (response.includes("ㅁ")) {
     addBadge("", "테이크아웃여부");
   }
 }
@@ -77,20 +77,21 @@ function search(keywords, callback) {
     API_KEY += "gdKBOT3BlbkFJo";
     API_KEY += "VSYTnt9wDHA3vDHgU4v";
 
-    reqStr = "A N/A\n";
-    reqStr += "B menu confirmed (Example: Iced Americano, Strawberry Latte, Matcha Frappuccino, Cheesecake, etc.)\n";
-    reqStr += "C size confirmed (Example: Tall, Tall size, Grande, Venti, Venti size, etc.)\n";
-    reqStr += "D Payment method confirmation (Example: card, cash, Kakao Pay, 10,000 won, 5,000 won, etc.)\n";
-    reqStr +="F Questions or requests to the clerk (e.g.: Can you recommend a menu?, what size do you have?, What time is it open here, etc.)\n";
+    reqStr = "ㄱ. N/A\n";
+    reqStr += "ㄴ. menu confirmed (Example: Iced Americano, Strawberry Latte, Matcha Frappuccino, Cheesecake, etc.)\n";
+    reqStr += "ㄷ. size confirmed (Example: Tall, Tall size, Grande, Venti, Venti size, etc.)\n";
+    reqStr += "ㄹ. Payment method confirmation (Example: card, cash, Kakao Pay, 10,000 won, 5,000 won, etc.)\n";
+    reqStr += "ㅁ. Confirmation of for here or to go (Example: store meals, takeout, eat-in, etc.)\n";
+    reqStr += "ㅂ. Questions or requests to the clerk (e.g.: Can you recommend a menu?, what size do you have?, What time is it open here, etc.)\n";
     reqStr += "The conversation below is a conversation between a customer and an employee at a cafe. \n";
     reqStr += "Choose all the correct answers from the examples above to indicate what the customer intended.\n";
-    reqStr += "Answer A or F for simple queries or just speaking on a related topic.\n";
+    reqStr += "Answer ㄱ or ㅂ for simple queries or just speaking on a related topic.\n";
     reqStr += "Don't say anything about irrelevant things.\n";
     reqStr += "======== 대화내용 =========\n";
     reqStr += lastMsg + "\n";
     reqStr += "손님 : " + keywords + "\n";
     reqStr += "===========================\n";
-    reqStr += "answer : ";
+    reqStr += "정답 : ";
 
 
   var config = {
