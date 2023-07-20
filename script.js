f67982bd3f6d64036429622b4c9675beb5fbaa44
jhlee3 <jhlee3@businessinsight.co.kr>
2023-07-20 17:14:34 +0900

    asdf

--- script.js
+++ script.js
@@ -8,25 +8,38 @@ var xhr = new XMLHttpRequest();
 var menuList;
 var askContent;
 
+// @TODO : chatgpt와 통신중인거 state 나타내는 div요소 하나 만들고(색깔 등)
+// 그사이에 채팅친거는 messages에 append해서 저장해놨다가 gpt에던지기
+
+var reqStr = "ㄱ. N/A\n";
+reqStr += "ㄴ. Menu decision (e.g. Iced Americano, Strawberry Latte, Matcha Frappuccino, Cheesecake, etc.)\n";
+reqStr += "ㄷ. Size decision (e.g. Tall, Tall size, Grande, Venti, Venti size, etc.)\n";
+reqStr += "ㄹ. Decide payment method (e.g. card, cash, Kakao Pay, 10,000 won, 5,000 won, etc.)\n";
+reqStr += "ㅁ. Decide for here or to go (e.g. to go, for here, eat-in, etc.)\n";
+reqStr += "ㅂ. Ask or request to the Staff (e.g. Can you recommend a menu?, what size do you have?, what menus can I order?, What time is it open here, etc.)\n";
+reqStr += "The conversation below is a conversation between a customer and an employee at a cafe. \n";
+reqStr += "Choose all the correct answers from the above choices to indicate what the customer decided.\n";
+reqStr += "Answer ㄱ or ㅂ if customer simply asked something or just told on a related topic.\n";
+reqStr += "Don't say anything about irrelevant things.\n";
+
 let API_KEY = "sk-wzWR5Z";
 API_KEY += "bfFqw1CT7";
 API_KEY += "gdKBOT3BlbkFJo";
 API_KEY += "VSYTnt9wDHA3vDHgU4v";
-var config = {
-    headers: {
-        Authorization: `Bearer ${API_KEY}`,
-        "Content-Type": "application/json",
-    },
-};
+var config = { headers: { Authorization: `Bearer ${API_KEY}`, "Content-Type": "application/json", }, };
+var data = { model: "gpt-3.5-turbo", temperature: 0.5, messages: '' };
+
+const setData = (messages) => { data.messages = messages; }
 
 const checkString = (str, listdata) => {
-    console.log(str,listdata)
+    console.log(str, listdata)
+    var result = [];
     for (let i = 0; i < listdata.length; i++) {
         if (str.includes(listdata[i])) {
-            return listdata[i];
+            result.append(listdata[i]);
         }
     }
-    return false;
+    return result;
 };
 
 xhr.open("GET", "data.json", true);
@@ -37,70 +50,85 @@ xhr.onreadystatechange = function () {
     }
 };
 xhr.send();
-
-
-
-
-$(document).ready(function () {
-    $("#reset-button").click(function () {
-        $("#chat-messages").empty();
-        $(".chat-header").empty();
-        $(".chat-header").append("Chat");
-        contents = [];
-        recentStaff = "직원 : 안녕하세요. Ri카페입니다. 무엇을 도와 드릴까요?";
-        recentCustomer = '';
-        $("#chat-messages").append(recentStaff); // 메세지 채팅창에 보이기
-    });
-
-    recentStaff = "직원 : 안녕하세요. Ri카페입니다. 무엇을 도와 드릴까요?";
+const initPage = () => {
+    $("#chat-messages").empty();
+    $(".chat-header").empty();
+    $(".chat-header").append("Chat");
+    contents = [];
+    recentStaff = "직원 : 안녕하세요. RI카페입니다. 무엇을 도와 드릴까요?";
+    recentCustomer = '';
     $("#chat-messages").append(recentStaff); // 메세지 채팅창에 보이기
-    // 엔터키 입력했을때 send-button 클릭이벤트
-    $("#message-input").keypress(function (e) {
-        if (e.which === 13) {
-            $("#send-button").click();
-            return false;
-        }
-    });
+}
 
-    // send-button click 이벤트
-    $("#send-button").click(function () {
-        var message = $("#message-input").val();
-        customerMsgForView = "나 : " + message;
-        recentCustomer += "손님 : " + message + "\n";
-        if (message.trim() !== "") {
-            clearTimeout(clickTimeout); // 클릭 타이머 초기화
-            var messageElement = $("<div>").text(customerMsgForView); // 메세지 채팅창에 보이기
-            $("#chat-messages").append(messageElement); // 메세지 채팅창에 보이기
-            $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
-            $("#message-input").val(""); // 메세지 input창 clear
-            clickTimeout = setTimeout(
-                search(),
-                checkTime * 1000
-            ); // checkTime 초동안 입력없으면 API 호출
-        }
-    });
-    //search(lastMsg, handleAPIResponse); // url, data, callback함수
+$("#reset-button").click(function () {
+    initPage();
 });
+$("#message-input").keypress(function (e) {
+    if (e.which === 13) {
+        $("#send-button").click();
+        return false;
+    }
+});
+$("#send-button").click(function () {
+    var message = $("#message-input").val();
+    customerMsgForView = "나 : " + message;
+    recentCustomer += "손님 : " + message;
+    if (message.trim() !== "") {
+        clearTimeout(clickTimeout); // 클릭 타이머 초기화
+        var messageElement = $("<div>").text(customerMsgForView); // 메세지 채팅창에 보이기
+        $("#chat-messages").append(messageElement); // 메세지 채팅창에 보이기
+        $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
+        $("#message-input").val(""); // 메세지 input창 clear
+        clickTimeout = setTimeout(search(), checkTime * 1); // checkTime 초동안 입력없으면 API 호출
+    }
+});
+$(document).ready(function () {
+    initPage();
+});
+
+const trimResponse = (response) => {
+    var result;
+    var textMsg = response.data.choices[0].message.content;
+    if (textMsg.indexOf("직원") != -1) {
+        result = textMsg;
+    } else {
+        result = "직원 : " + textMsg;
+    }
+    if (result.indexOf("손님 :") !== -1) {
+        result = result.substring(0, index1);
+    }
+    if (result.indexOf("손님:") !== -1) {
+        result = result.substring(0, index2);
+    }
+    return result;
+}
+
+
 const handleAPIResponse = async (response) => {
-// function async handleAPIResponse(response) {
+    // function async handleAPIResponse(response) {
     var question = recentStaff + "\n" + recentCustomer;
     if (response.includes("ㄴ")) {
-        question += "\n위 대화를 보고 다음 보기 중 손님이 고른 메뉴를 고르시오\n";
+        question += "\nLook at the above conversation and select the menu the customer chose from the following lists.\n";
+        question += "\nIf the menu selected by the guest is not in the view below, say None.\n";
         question += JSON.stringify(menuList);
         question += "\n answer : "
         console.log(question)
         var getResult = await getGpt(question)
-        deciMenu = checkString(getResult, menuList)
+        deciMenu = checkString(getResult, menuList) // 검사해서 첫번째로 겹치는거
         console.log(deciMenu);
-        if (deciMenu != false){
+        if (deciMenu != []) {
             addBadge("", "메뉴선택");
-            addBadge("",deciMenu)
+            for (var i = 0; i < deciMenu.length; i++) {
+                addBadge("", deciMenu[i])
+            }
             askContent = "";
-        } 
-        else{
-            askContent = JSON.stringify(menuList);
-            askContent += "You said that the customer ordered "+deciMenu
-            +", but there is no such menu. Serve customers by referring to the above menu board "
+        }
+        else {
+            askContent = "============메뉴판===========\n"
+            askContent += JSON.stringify(menuList);
+            askContent += "\n===========================\n"
+            askContent += "Customer ordered [" + getResult
+                + "], but there is no such menu. Serve customers by simply sentense. "
         }
     }
     if (response.includes("ㄷ")) {
@@ -126,7 +154,7 @@ function addBadge(cls, content) {
             $("." + cls).append('<div class="badge">' + content + "</div>");
         }
     }
-    
+
 }
 
 const getGpt = async (keyword) => {
@@ -137,11 +165,8 @@ const getGpt = async (keyword) => {
         },
         { role: "user", content: keyword },
     ];
-    var data = {
-        model: "gpt-3.5-turbo",
-        temperature: 0.5,
-        messages: messages,
-    };
+
+    setData(messages)
 
     try {
         const response = await axios.post("https://api.openai.com/v1/chat/completions", data, config);
@@ -155,51 +180,24 @@ const getGpt = async (keyword) => {
 }
 
 function search() {
-    reqStr =
-        "ㄱ. N/A\n";
-    reqStr +=
-        "ㄴ. Menu decision (e.g. Iced Americano, Strawberry Latte, Matcha Frappuccino, Cheesecake, etc.)\n";
-    reqStr +=
-        "ㄷ. Size decision (e.g. Tall, Tall size, Grande, Venti, Venti size, etc.)\n";
-    reqStr +=
-        "ㄹ. Decide payment method (e.g. card, cash, Kakao Pay, 10,000 won, 5,000 won, etc.)\n";
-    reqStr +=
-        "ㅁ. Decide for here or to go (e.g. to go, for here, eat-in, etc.)\n";
-    reqStr +=
-        "ㅂ. Ask or request to the Staff (e.g. Can you recommend a menu?, what size do you have?, what menus can I order?, What time is it open here, etc.)\n";
-    reqStr +=
-        "The conversation below is a conversation between a customer and an employee at a cafe. \n";
-    reqStr +=
-        "Choose all the correct answers from the above choices to indicate what the customer decided.\n";
-    reqStr +=
-        "Answer ㄱ or ㅂ if customer simply asked something or just told on a related topic.\n";
-    reqStr += "Don't say anything about irrelevant things.\n";
-    reqStr += "======== 대화내용 =========\n";
-    reqStr += recentStaff + "\n";
-    reqStr += recentCustomer;
-    reqStr += "===========================\n";
-    reqStr += "Answer : ";
+    var getIntendScript = reqStr + "======== 대화내용 =========\n";
+    getIntendScript += recentStaff + "\n";
+    getIntendScript += recentCustomer;
+    getIntendScript += "===========================\n";
+    getIntendScript += "Answer : ";
 
     var messages = [
-        {
-            role: "system",
-            content: "Act as a person who chooses the correct answer",
-        },
-        { role: "user", content: reqStr },
+        { role: "system", content: "Act as a person who chooses the correct answer", },
+        { role: "user", content: getIntendScript },
     ];
-    var data = {
-        model: "gpt-3.5-turbo",
-        temperature: 0.5,
-        messages: messages,
-    };
+    setData(messages);
 
     axios
         .post("https://api.openai.com/v1/chat/completions", data, config) // POST 요청
         .then(function (response) {
-            console.log(reqStr);
+            console.log(getIntendScript);
             console.log(response.data.choices[0].message.content);
             handleAPIResponse(response.data.choices[0].message.content);
-            
         })
         .catch(function (error) {
             console.error(error);
@@ -207,7 +205,6 @@ function search() {
 }
 
 function StaffRespond() {
-
     keywords = recentStaff + "\n" + recentCustomer;
     let nextAskContent = "";
     if (contents.indexOf("메뉴선택") == -1) {
@@ -225,7 +222,7 @@ function StaffRespond() {
 
     if (nextAskContent !== "" && askContent == "") {
         keywords += "The above is just a conversation between a cafe Staff and a customer.";
-        keywords += "You should be a kind staff. And ask about" + nextAskContent + "to Customer by the language requested by the Customer.\n";
+        keywords += "Ask about" + nextAskContent + "to customer by the language requested by the Customer.\n";
         keywords += "직원 : "
         var messages = [
             {
@@ -234,70 +231,31 @@ function StaffRespond() {
             },
             { role: "user", content: keywords },
         ];
-        var data = {
-            model: "gpt-3.5-turbo",
-            temperature: 0.5,
-            messages: messages,
-        };
+        setData(messages);
         console.log(keywords);
         axios.post("https://api.openai.com/v1/chat/completions", data, config) // POST 요청
             .then(function (response) {
                 console.log(response.data.choices[0].message.content);
-                if (
-                    response.data.choices[0].message.content.indexOf("직원") != -1
-                ) {
-                    recentStaff = response.data.choices[0].message.content;
-                } else {
-                    recentStaff = "직원 : " + response.data.choices[0].message.content;
-                }
-                var index1 = recentStaff.indexOf("손님 :");
-                var index2 = recentStaff.indexOf("손님:");
-                if (index1 !== -1) {
-                    recentStaff = recentStaff.substring(0, index1);
-                }
-                if (index2 !== -1) {
-                    recentStaff = recentStaff.substring(0, index2);
-                }
+                recentStaff = trimResponse(response)
                 $("#chat-messages").append(recentStaff); // 메세지 채팅창에 보이기
                 $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
             })
             .catch(function (error) {
                 console.error(error);
             });
-    } else if (askContent != ""){
+    } else if (askContent != "") {
         keywords = askContent
         keywords += "직원 : "
         var messages = [
-            {
-                role: "system",
-                content: "you are a cafe Staff. Treat guests kindly.",
-            },
+            { role: "system", content: "you are a cafe Staff. Treat guests kindly." },
             { role: "user", content: keywords },
         ];
-        var data = {
-            model: "gpt-3.5-turbo",
-            temperature: 0.5,
-            messages: messages,
-        };
+        setData(messages);
         console.log(keywords);
         axios.post("https://api.openai.com/v1/chat/completions", data, config) // POST 요청
             .then(function (response) {
                 console.log(response.data.choices[0].message.content);
-                if (
-                    response.data.choices[0].message.content.indexOf("직원") != -1
-                ) {
-                    recentStaff = response.data.choices[0].message.content;
-                } else {
-                    recentStaff = "직원 : " + response.data.choices[0].message.content;
-                }
-                var index1 = recentStaff.indexOf("손님 :");
-                var index2 = recentStaff.indexOf("손님:");
-                if (index1 !== -1) {
-                    recentStaff = recentStaff.substring(0, index1);
-                }
-                if (index2 !== -1) {
-                    recentStaff = recentStaff.substring(0, index2);
-                }
+                recentStaff = trimResponse(response);
                 $("#chat-messages").append(recentStaff); // 메세지 채팅창에 보이기
                 $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
             })
@@ -306,7 +264,7 @@ function StaffRespond() {
             });
 
     }
-    
+
     else {
         recentStaff = "직원 : 주문이 모두 완료되었습니다. 잠시만 기다려주세요!";
         $("#chat-messages").append(recentStaff);
