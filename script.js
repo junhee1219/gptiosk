9961fba823ba5813db0d848f69c2702d21c990fe
jhlee3 <jhlee3@businessinsight.co.kr>
2023-08-01 14:21:08 +0900

    asdf

--- script.js
+++ script.js
@@ -13,19 +13,15 @@ let payMethodList = ["카카오페이", "카드", "현금", "네이버페이"]
 let hereOrTogo = ["테이크아웃", "매장식사"]
 let deciList = [];
 
-
-// @TODO : chatgpt와 통신중인거 state 나타내는 div요소 하나 만들고(색깔 등)
-// 그사이에 채팅친거는 messages에 append해서 저장해놨다가 gpt에던지기
 let reqStr = '';
 reqStr += "ㄱ. 손님 decided coffee menu to order.\n";
-reqStr += "ㄴ. 손님 decided coffee size(not quantity).\n";
+reqStr += "ㄴ. 손님 decided coffee size.\n";
 reqStr += "ㄷ. 손님 decided payment method to pay.\n";
 reqStr += "ㄹ. 손님 decided for here or to go.\n";
-reqStr += "ㅁ. 손님 decided quantity of coffee.\n";
-reqStr += "ㅂ. 손님 just ask or request to the staff.(recommendation, information, etc..)\n";
-reqStr += "ㅅ. Others.(ice breaking, small talk, etc..)\n";
+reqStr += "ㅁ. 손님 just ask or request to the staff.(recommendation, information, etc..)\n";
+reqStr += "ㅂ. Others.(ice breaking, small talk, etc..)\n";
 reqStr += "The below is a conversation between 직원 and 손님 at a cafe.\n";
-reqStr += "Choose all the correct answer or ㅂ about 손님.\n";
+reqStr += "Choose only correct answers about 손님 and why you think that simply.\n";
 reqStr += "If it is a simple question or just small talk, answer only ㅁ or ㅂ\n";
 
 
